2f9f12e5f2d7bfafb68f46c5218432a29a753239
mauriciomarcos.consultoria@gmail.com <mauriciomarcos.consultoria@gmail.com>
2020-02-26 18:19:37 -0300

    Criação do projeto e criação de request GetAsync.

---
+++ WebAPIClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebAPIClient.Models;
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(async () =>
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri("https://localhost:44311/");
+
+                var response = await client.GetAsync("/api/tarefa");
+                var getAllTarefas = await response.Content.ReadAsAsync<IEnumerable<Tarefa>>();
+
+            })
+            .GetAwaiter()
+            .GetResult();
+        }
+    }
+}
